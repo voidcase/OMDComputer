c858e46ad3bcdaf43f4f8d1352857a04a9cee445
voidcase <dat13ili@student.lu.se>
2014-09-20 23:53:11 +0200

    finished up jumpers

--- src/os/Command.java
+++ src/os/Command.java
@@ -1,5 +1,5 @@
 package os;
 
 public interface Command {
-	public abstract void execute(Memory m, ProgramCounter pc);
+	public abstract void execute(Memory m, ProgramCounter pc);	//TODO osäker om den ska vara abstract.
 }
