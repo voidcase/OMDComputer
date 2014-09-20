ebaf7e0d8bf0923780cf18bab9a9551d4116d0fb
voidcase <dat13ili@student.lu.se>
2014-09-20 23:35:40 +0200

    reduced duplicated code

--- src/os/SuperJump.java
+++ src/os/SuperJump.java
@@ -1,5 +1,9 @@
 package os;
 
 public abstract class SuperJump implements Command{
+	protected int destination;
+	public SuperJump(int d){
+		destination = d;
+	}
 	public abstract void execute(Memory m, ProgramCounter pc);
 }
