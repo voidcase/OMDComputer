f4bb3835c57cdfaf411496359c189695d6b09556
Erik Grampp <dat13egr@jupiter-1.student.lth.se>
2014-09-22 16:16:05 +0200

    Added Mul and Add and toString for both

--- src/os/ArithmeticOp.java
+++ src/os/ArithmeticOp.java
@@ -4,10 +4,26 @@ public abstract class ArithmeticOp implements Command {
 	protected Operand m1, m2;
 	protected Address m3;
 
-	protected abstract LongWord ArithmaticOp (Operand first, Operand second);
+	protected abstract String nameOf();
 	
-	public void execute(Memory m, ProgramCounter pc) {
-		m.edit(ArithmaticOp(m1.getWord(m), m2.getWord(m)), m3);
+	public String toString() {
+		
+		StringBuilder tempS = new StringBuilder();
+		tempS.append(nameOf());
+		tempS.append("\t");
+		tempS.append(m1.toString());
+		tempS.append("\t");
+		tempS.append(m2.toString());
+		tempS.append("\t");
+		tempS.append(m3.toString());
+		return tempS.toString();
+	}
+	
+	protected abstract Word ArithmaticOp (Operand first, Operand second, Memory m);
+	
+	public int execute(Memory m, int pc) {
+		m.edit(ArithmaticOp(m1.getWord(m), m2.getWord(m), m), m3);
+		return pc++;
 	}
 
 	public ArithmeticOp (Operand m1, Operand m2, Address m3) {
